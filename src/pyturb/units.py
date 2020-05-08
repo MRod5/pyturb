a427c8ee5a7d8e2a3fd7ae43c56eec1c76db4734
MRodriguez <marcosrodriguezjimenez@gmail.com>
2020-05-08 14:06:24 +0200

    Refactored all variables from oldunit2newunit to oldunit_to_newunit, for readibility

--- src/pyturb/units.py
+++ src/pyturb/units.py
@@ -9,123 +9,160 @@ MRodriguez
 from numpy import pi
 
 ## Constants:
-grav = 9.80665      # Gravitational acceleration [m/s^2]
-Ru = 8.314472       # Universal gas constant [J/mol/K]
-p_ref_SL = 101325   # Reference pressure at sea level [Pa]
+grav = 9.80665          # Gravitational acceleration [m/s^2]
+Ru = 8.314472           # Universal gas constant [J/mol/K]
+p_ref_SL = 101325       # Reference pressure at sea level [Pa]
 
 
 ## Units conversion: 
 #Angles:
-deg2rad = pi/180    # Degrees to radians
-rad2deg = 180/180   # Radians to degrees
+deg_to_rad = pi/180     # Degrees to radians
+rad_to_deg = 180/180    # Radians to degrees
 
 
 # Distance:
-ft2m = 0.3048       # feet to meters
-m2ft = 1/ft2m       # meters to ft
-FL2ft = 100         # Flight level to ft
-in2m = 0.0254       # Inches to meters
-m2in = 1/in2m       # Meters to inches
-nm2m = 1852         # Nautical mile to meters
-m2nm = 1/nm2m       # Meters to nautical miles
+ft_to_m = 0.3048        # feet to meters
+m_to_ft = 1/ft_to_m     # meters to ft
+FL_to_ft = 100          # Flight level to ft
+FL_to_m = FL_to_ft*ft_to_m # Flight level to ft
+in_to_m = 0.0254        # Inches to meters
+m_to_in = 1/in_to_m     # Meters to inches
+nm_to_m = 1852          # Nautical mile to meters
+m_to_nm = 1/nm_to_m     # Meters to nautical miles
 
 
 # Speed:
-kts2ms = 1852/3600  # A Knot is a nautical mile per hour. Conversion to meters per second is exact this way.
-ms2kts = 1/kts2ms   # Meters per second to knots
-mph2ms = 1609.344/3600 # Statute  mile per hour to meters per second
-ms2mph = 1/mph2ms # Meters per second to statute miles per hour
+kts_to_ms = 1852/3600   # A Knot is a nautical mile per hour. Conversion to meters per second is exact this way.
+ms_to_kts = 1/kts_to_ms   # Meters per second to knots
+mph_to_ms = 1609.344/3600 # Statute  mile per hour to meters per second
+ms_to_mph = 1/mph_to_ms # Meters per second to statute miles per hour
 
 
 # Mass:
-lb2kg = 0.45359237  # Pounds (Avoirdupois) to kilograms
-kg2lb = 1/lb2kg     # Kilograms to Pounds (Avoirdupois)
-slug2kg = grav*m2ft*lb2kg # Slugs to kilograms
-kg2slug = 1/slug2kg # Kilograms to slugs
+lb_to_kg = 0.45359237   # Pounds (Avoirdupois) to kilograms
+kg_to_lb = 1/lb_to_kg   # Kilograms to Pounds (Avoirdupois)
+slug_to_kg = grav*m_to_ft*lb_to_kg # Slugs to kilograms
+kg_to_slug = 1/slug_to_kg # Kilograms to slugs
 
 
 # Force:
-kp2N = grav         # Kilopond to Newton
-N2kp = 1/grav       # Newtons to Kiloponds
-lbf2kp = lb2kg      # Pound-force to kiloponds
-kp2lb = 1/lb2kg     # Kiloponds to pound-force
-lbf2N = lbf2kp*kp2N # Pound-force to Newtons
-N2lbf = 1/lbf2N     # Pound-force to Newtons
+kp_to_N = grav          # Kilopond to Newton
+N_to_kp = 1/grav        # Newtons to Kiloponds
+lbf_to_kp = lb_to_kg    # Pound-force to kiloponds
+kp_to_lb = 1/lb_to_kg   # Kiloponds to pound-force
+lbf_to_N = lbf_to_kp*kp_to_N # Pound-force to Newtons
+N_to_lbf = 1/lbf_to_N   # Pound-force to Newtons
 
 
 # Pressure:
-bar2Pa = 1e5        # Bar to Pascals
-Pa2bar = 1e-5       # Pascals to bar
-mbar2Pa = 1e-3*bar2Pa # mbar (mili bar) to Pascal
-Pa2mbar = 1e+3*Pa2bar # Pascal to mbar (mili bar)
-atm2Pa = p_ref_SL   # Atmospheres to Pascals
-Pa2atm = 1/atm2Pa   # Pascals to Atmospheres
-Torr2Pa = p_ref_SL/760 # Torr (mmHg) to Pascals
-Pa2Torr = 1/Torr2Pa # Torr (mmHg) to Pascals
-mmHg2Pa = Torr2Pa   # Torr = mmHg
-Pa2mmHg = Pa2Torr   # Torr = mmHg
+bar_to_Pa = 1e5         # Bar to Pascals
+Pa_to_bar = 1e-5        # Pascals to bar
+mbar_to_Pa = 1e-3*bar_to_Pa # mbar (mili bar) to Pascal
+Pa_to_mbar = 1e+3*Pa_to_bar # Pascal to mbar (mili bar)
+atm_to_Pa = p_ref_SL    # Atmospheres to Pascals
+Pa_to_atm = 1/atm_to_Pa # Pascals to Atmospheres
+Torr_to_Pa = p_ref_SL/760 # Torr (mmHg) to Pascals
+Pa_to_Torr = 1/Torr_to_Pa # Torr (mmHg) to Pascals
+mmHg_to_Pa = Torr_to_Pa # Torr = mmHg
+Pa_to_mmHg = Pa_to_Torr # Torr = mmHg
 
 
 # Energy:
-kWh2J = 3.6e6       # kilowatt-hour to Joules
-J2kWh = 1/kWh2J     # Joules to kilowatt-hour
-cal2J = kWh2J/860*1e-3 # Calories (international steam table) to joules
-J2cal = 1/cal2J     # Joules to calories (international steam table)
-Btu2J =  1055.06    # BTU (British thermal unit - ISO) to Joules
-J2Btu = 1/Btu2J     # Joules to BTU (ISO)
+kWh_to_J = 3.6e6        # kilowatt-hour to Joules
+J_to_kWh = 1/kWh_to_J   # Joules to kilowatt-hour
+cal_to_J = kWh_to_J/860*1e-3 # Calories (international steam table) to joules
+J_to_cal = 1/cal_to_J   # Joules to calories (international steam table)
+Btu_to_J =  1055.06     # BTU (British thermal unit - ISO) to Joules
+J_to_Btu = 1/Btu_to_J   # Joules to BTU (ISO)
 
 
 # Power:
-hp2W = 550 * lbf2N * ft2m # Horsepower (mechanical) to Watts
-W2hp = 1/hp2W       # Watts to Horsepower (mechanical)
-metric_hp2W = 75 * kp2N  # Horsepower (metric) to Watts
-W2hp_metric = 1/metric_hp2W       # Watts to Horsepower (metric)
+hp_to_W = 550 * lbf_to_N * ft_to_m  # Horsepower (mechanical) to Watts
+W_to_hp = 1/hp_to_W                 # Watts to Horsepower (mechanical)
+metric_hp_to_W = 75 * kp_to_N       # Horsepower (metric) to Watts
+W_to_hp_metric = 1/metric_hp_to_W   # Watts to Horsepower (metric)
 
 
 # Temperature:
-def celsius2kelvin(temp):
+def celsius_to_kelvin(temp):
+    """
+    From Celsius (ºC) to Kelvin (K)
+    """
     return temp + 273.15
 
 
-def kelvin2celsius(temp):
+def kelvin_to_celsius(temp):
+    """
+    From Kelvin (K) to Celsius (ºC)
+    """
     return temp - 273.15
 
 
-def celsius2fahrenheit(temp):
+def celsius_to_fahrenheit(temp):
+    """
+    From Celsius (ºC) to Fahrenheit (ºF)
+    """
     return temp*9/5 + 32
 
 
-def fahrenheit2celsius(temp):
+def fahrenheit_to_celsius(temp):
+    """
+    From Fahrenheit (ºF) to Celsius (ºC)
+    """
     return (temp - 32)*5/9
 
 
-def fahrenheit2rankine(temp):
+def fahrenheit_to_rankine(temp):
+    """
+    From Fahrenheit (ºF) to Rankine (R)
+    """
     return temp + 459.67
 
 
-def rankine2fahrenheit(temp):
+def rankine_to_fahrenheit(temp):
+    """
+    From Rankine (R) to Fahrenheit (ºF)
+    """
     return temp - 459.67
 
 
-def fahrenheit2kelvin(temp):
-    return celsius2kelvin(fahrenheit2celsius(temp))
+def fahrenheit_to_kelvin(temp):
+    """
+    From Fahrenheit (ºF) to Kelvin (K)
+    """
+    return celsius_to_kelvin(fahrenheit_to_celsius(temp))
 
 
-def kelvin2fahrenheit(temp):
-    return celsius2fahrenheit(kelvin2celsius(temp))
+def kelvin_to_fahrenheit(temp):
+    """
+    From Kelvin (K) to Fahrenheit (ºF)
+    """
+    return celsius_to_fahrenheit(kelvin_to_celsius(temp))
 
 
-def celsius2rankine(temp):
-    return fahrenheit2rankine(celsius2fahrenheit(temp))
+def celsius_to_rankine(temp):
+    """
+    From Celsius (ºC) to Rankine (R)
+    """
+    return fahrenheit_to_rankine(celsius_to_fahrenheit(temp))
 
 
-def rankine2celsius(temp):
-    return fahrenheit2celsius(rankine2fahrenheit(temp))
+def rankine_to_celsius(temp):
+    """
+    From Rankine (R) to Celsius (ºC)
+    """
+    return fahrenheit_to_celsius(rankine_to_fahrenheit(temp))
 
 
-def kelvin2rankine(temp):
+def kelvin_to_rankine(temp):
+    """
+    From Kelvin (K) to Rankine (R)
+    """
     return temp*9/5
 
 
-def rankine2kelvin(temp):
+def rankine_to_kelvin(temp):
+    """
+    From Rankine (R) to Kelvin (K)
+    """
     return temp*5/9
