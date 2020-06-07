f914649ff9d33f1b0d3551dea7a3c3ec068116cc
MRodriguez <marcosrodriguezjimenez@gmail.com>
2020-06-07 13:19:44 +0200

    Perfect ideal gas is always calculated at T_ref (25ºC), with cp, cv gamma constants

--- src/pyturb/gas_models/perfect_ideal_gas.py
+++ src/pyturb/gas_models/perfect_ideal_gas.py
@@ -3,6 +3,7 @@
 
 import pyturb.utils.constants as cts
 from pyturb.gas_models.gas import Gas
+from pyturb.gas_models.semiperfect_ideal_gas import SemiperfectIdealGas
 from pyturb.gas_models.thermo_properties import ThermoProperties
 
 
@@ -20,11 +21,16 @@ class PerfectIdealGas(Gas):
             raise TypeError("Gas species must be a string")
 
 
-        self.thermo_prop = ThermoProperties(species)
-        if self.thermo_prop.Mg == None:
-            raise ValueError("Gas unavailable: {}".format(species))
-        else:
-            self.__gas_species = species
+        # self.thermo_prop = ThermoProperties(species)
+        # if self.thermo_prop.Mg == None:
+        #     raise ValueError("Gas unavailable: {}".format(species))
+        # else:
+        #     self.__gas_species = species
+
+        self.__from_semiperfect_gas = SemiperfectIdealGas(species)
+        self.thermo_prop = self.__from_semiperfect_gas.thermo_prop
+        self.T_ref = cts.T_ref
+        self.__gas_species = species
         return
         
     
@@ -45,17 +51,18 @@ class PerfectIdealGas(Gas):
         return Rg
         
     
-    def cp(self, temperature):
-        return 1004
+    def cp(self, temperature=None):
+        cp_ = self.__from_semiperfect_gas.cp(self.T_ref)
+        return cp_
         
     
-    def cv(self, temperature):
-        cv_ = self.cp(temperature) - self.Rg
+    def cv(self, temperature=None):
+        cv_ = self.cp(self.T_ref) - self.Rg
         return cv_
     
         
-    def gamma(self, temperature):
-        gamma_ = self.cp(temperature)/self.cv(temperature)
+    def gamma(self, temperature=None):
+        gamma_ = self.cp(self.T_ref)/self.cv(self.T_ref)
         return gamma_
         
         
