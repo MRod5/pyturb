e734c692e8f1b9548ab76587716338f4620042b3
MRodriguez <marcosrodriguezjimenez@gmail.com>
2020-06-10 21:19:03 +0200

    Superclass with Fluid objected, inherited by all stages in power_plant

---
+++ src/pyturb/fluid.py
@@ -0,0 +1,46 @@
+"""
+"""
+
+from abc import abstractmethod
+from pyturb.gas_models.perfect_ideal_gas import PerfectIdealGas
+from pyturb.gas_models.semiperfect_ideal_gas import SemiperfectIdealGas
+
+
+
+class Fluid(object):
+    """
+    """
+    def __init__(self, fluid):
+        if not(isinstance(fluid, PerfectIdealGas) or isinstance(fluid, SemiperfectIdealGas)):
+            raise TypeError("PerfectIdealGas, SemiperfectIdealGas")
+        self.fluid = fluid
+        return
+
+    @abstractmethod
+    def het(self):
+        """
+        """
+        raise NotImplementedError
+
+
+    @abstractmethod
+    def hst(self):
+        """
+        """
+        raise NotImplementedError
+
+    
+    @property
+    @abstractmethod
+    def Tet(self):
+        """
+        """
+        raise NotImplementedError
+
+
+    @property
+    @abstractmethod
+    def Tst(self):
+        """
+        """
+        raise NotImplementedError
