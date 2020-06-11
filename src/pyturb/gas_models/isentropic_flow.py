06e23b4a207a7148335778a29c6faaf824fac171
MRodriguez <marcosrodriguezjimenez@gmail.com>
2020-06-11 19:34:44 +0200

    Added dynamic pressure, impact pressure, velocity from mach and stagnation relations and stagnation density

--- src/pyturb/gas_models/isentropic_flow.py
+++ src/pyturb/gas_models/isentropic_flow.py
@@ -214,3 +214,212 @@ class IsentropicFlow(object):
         pt = p * (Tt_T)**(gamma/(gamma-1))
 
         return pt
+
+
+    def impact_pressure_from_mach(self, mach, static_pressure, static_temperature=None):
+        """
+        Impact pressure given the Mach number and the local static pressure.
+
+        Inputs:
+        -------
+            mach: float. Mach number of the fluid [non-dimensional]
+            static_pressure: float. Local static pressure [Pa]
+            static_temperature: float. Static temperature of the gas [K]
+
+        Outputs:
+        --------
+            qi: float. Impact pressure [Pa]
+
+        """
+
+        if not 0<static_pressure:
+            raise ValueError("Static pressure {}Pa cannot be negative".format(static_pressure))
+        else:
+            p = static_pressure
+
+        pt = self.stag_pressure_from_mach(mach, p, static_temperature)        
+        qi = pt - p
+        
+        return qi
+
+
+    def dynamic_pressure(self, vel, static_density):
+        """
+        Calculates the dynamic pressure of the flow, provided its static density
+        and the velocity of the flow.
+        
+        Note that if the flow can be considered incompressible (isochoric), thus
+        the Mach number of the flow is less than 0.3, then the stagnation pressure
+        can be obtained as the static pressure plus the dynamic pressure:
+            if M<0.3, then t = p + qi
+        
+        Inputs:
+        -------
+            vel: float. Flow velocity [m/s]
+            static_density: float. Local static density [kg/m**3]
+
+        Outputs:
+        --------
+            qi: float. Dynamic pressure of the flow [Pa]
+
+        """
+
+        if not 0<static_density:
+            raise ValueError("Static density {}kg/m**3 cannot be negative".format(static_density))
+        else:
+            rho = static_density
+
+        qi = 0.5*rho*vel**2
+
+        return qi
+        
+
+    def dynamic_pressure_from_statpress(self, mach, static_pressure, static_temperature=None):
+        """
+        Calculates the dynamic pressure of the flow, provided its static pressure
+        and the Mach number.
+        
+        Note that if the flow can be considered incompressible (isochoric), thus
+        the Mach number of the flow is less than 0.3, then the stagnation pressure
+        can be obtained as the static pressure plus the dynamic pressure:
+            if M<0.3, then t = p + qi
+        
+        Inputs:
+        -------
+            mach: float. Mach number of the fluid [non-dimensional]
+            static_pressure: float. Local static pressure [Pa]
+            static_temperature: float. Static temperature of the gas [K]
+                If no static temperature is provided T_ref=298.15K is 
+                taken if fluid is Perfect gas, otherwise a valid static 
+                temperature must be provided.
+
+        Outputs:
+        --------
+            qi: float. Dynamic pressure of the flow [Pa]
+
+        """
+
+        if static_temperature is None:
+            if not isinstance(self.fluid, PerfectIdealGas):
+                raise ValueError("A valid static temperature must be provided")
+            else:
+                gamma = self.fluid.gamma()
+        else:
+            gamma = self.fluid.gamma(static_temperature)
+
+        if not 0<static_pressure:
+            raise ValueError("Static pressure {}Pa cannot be negative".format(static_pressure))
+        else:
+            p = static_pressure
+
+        qi = gamma/2 * p * mach**2
+
+        return qi
+
+
+    def vel_from_stag_temp(self, stagnation_temperature, static_temperature):
+        """
+        Flow velocity from the kinetic energy of the fluid, as the difference
+        between the stagnation and static temperatures.
+
+        Inputs:
+        -------
+            stagnation_temperature: float. Stagnation temperature of the gas [K]
+            static_temperature: float. Static temperature of the gas [K]
+
+        Outputs:
+        --------
+            vel: float. Flow velocity [m/s]
+
+        """
+
+        if not (0<static_temperature<6000 and 0<stagnation_temperature<6000):
+            raise ValueError("A valid temperatures must be provided.")
+        elif not stagnation_temperature>static_temperature:
+            raise ValueError("Stagnation temperature must be greater than static temperature.")
+        else:
+            T = static_temperature
+            Tt = stagnation_temperature
+            
+
+        difcpT = (self.fluid.cp(Tt)*Tt - self.fluid.cp(T)*T)
+        vel = np.sqrt(2*difcpT)
+        
+        return vel
+
+
+    def vel_from_mach(self, mach, static_temperature):
+        """
+        Flow velocity from the Mach number of the fluid.
+
+        Inputs:
+        -------
+            mach: float. Mach number [dimensionless]
+            static_temperature: float. Static temperature of the gas [K]
+
+
+        Outputs:
+        --------
+            vel: float. Flow velocity [m/s]
+
+        """
+
+        if not 0<static_temperature<6000:
+            raise ValueError("Static temperature {}K out of bounds [0-6000]K".format(static_temperature))
+        else:
+            T = static_temperature
+
+        a = self.sound_speed(T)
+        vel = a*mach
+
+        return vel
+
+
+    def stag_density_from_mach(self, mach, static_density, static_temperature=None):
+        """
+        Calculates the stagnation density given the Mach number and the local 
+        static density.
+
+        In case a semiperfect gas is used (gamma is a function of the temperature)
+        a temperature must be provided to gather the heat capacity ratio.
+
+        Inputs:
+        -------
+            mach: float. Mach number of the fluid [non-dimensional]
+            static_density: float. Local static density [kg/m**3]
+            static_temperature: float. Static temperature of the gas [K]
+                If no static temperature is provided T_ref=298.15K is 
+                taken if fluid is Perfect gas, otherwise a valid static 
+                temperature must be provided.
+
+        Outputs:
+        --------
+            rhot: float. stagnation density [Pa]
+
+        """
+
+        if static_temperature is None:
+            if not isinstance(self.fluid, PerfectIdealGas):
+                raise ValueError("A valid static temperature must be provided")
+            else:
+                gamma = self.fluid.gamma()
+                T = None
+        else:
+            gamma = self.fluid.gamma(static_temperature)
+
+            if not 0<static_temperature<6000:
+                raise ValueError("Static temperature {}K out of bounds [0-6000]K".format(static_temperature))
+            else:
+                T = static_temperature
+
+        if not 0<static_density:
+            raise ValueError("Static density {}kg/m**3 cannot be negative".format(static_density))
+        else:
+            rho = static_density
+
+        Tt_T = self.stagnation_static_rel(mach, T)
+        rhot = rho * (Tt_T)**(1/(gamma-1))
+
+        return rhot
+
+    
