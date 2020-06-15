85842cbef70ba58d85844ec768879ff6d0245de7
MRodriguez <marcosrodriguezjimenez@gmail.com>
2020-06-15 20:14:07 +0200

    ControlVolume properties implemented in nozzle.py

--- src/pyturb/power_plant/nozzle.py
+++ src/pyturb/power_plant/nozzle.py
@@ -13,22 +13,295 @@ MRodriguez 2020
 """
 
 from pyturb.power_plant.control_volume import ControlVolume
+from pyturb.gas_models.isentropic_flow import IsentropicFlow
+from pyturb.gas_models.perfect_ideal_gas import PerfectIdealGas
+from pyturb.gas_models.semiperfect_ideal_gas import SemiperfectIdealGas
+#from sympy import symbols, Eq, solveset, S
+#import numpy as np
 
 class Nozzle(ControlVolume):
     """
     """
-    def __init__(self, stage, fluid):
+    def __init__(self, fluid, stage=None):
         """
         """
+        # Stage of the power plant:
         self.stage = stage
-        super().__init__(fluid)
-            
-        return
         
-    def solve(self):
+        # Initialize superclass methods
+        super().__init__(fluid)
+
+        # Isentropic flow:
+        if not(isinstance(fluid, PerfectIdealGas) or isinstance(fluid, SemiperfectIdealGas)):
+            # Check the flow is a Perfect or a Semiperfect gas fom pyturb
+            raise TypeError("Object must be PerfectIdealGas, SemiperfectIdealGas. Instead received {}".format(fluid))
+    
+        self.isent_flow = IsentropicFlow(self.fluid)
+
+            
+        return None
+
+    # **************************
+    # *** Entrance of the CV *** 
+    # **************************
+    # Static thermodynamic properties:
+    @property
+    def p_e(self):
+        """
+        Static pressure at the entrance of the CV. [Pa]
+        """
+        return self._p_e
+    
+
+    @property
+    def T_e(self):
+        """
+        Static temperature at the entrance of the CV. [K]
+        """
+        return self._T_e
+    
+
+    @property
+    def rho_e(self):
+        """
+        Static density at the entrance of the CV. [kg/m**3]
+        """
+        return self._rho_e
+
+
+    @property
+    def h_e(self):
+        """
+        Specific static enthalpy at the entrance of the CV. [W/kg/s]
+        """
+        return self._h_e
+
+
+    # Stagnation thermodynamic properties
+    @property
+    def p_et(self):
+        """
+        Stagnation pressure at the entrance of the CV. [Pa]
+        """
+        return self._p_et
+
+
+    @property
+    def T_et(self):
+        """
+        Stagnation temperature at the entrance of the CV. [K]
+        """
+        return self._T_et
+
+
+    @property
+    def rho_et(self):
+        """
+        Stagnation density at the entrance of the CV. [kg/m**3]
+        """
+        return self._rho_et
+
+
+    @property
+    def h_et(self):
+        """
+        Specific stagnation enthalpy at the entrance of the CV. [W/kg/s]
+        """
+        return self._h_et
+
+
+    # Velocity and kinetic energy:
+    @property
+    def vel_e(self):
+        """
+        Flow velocity at the entrance of the CV. [m/s]
+        """
+        return self._vel_e
+
+
+    @property
+    def mach_e(self):
         """
         """
-        print('solve nozzle')
-        self.solved = True
-        print(self.fluid.cp())
-        return11
+        return self._mach_e
+    
+
+    @property
+    def ekin_e(self):
+        """
+        Specific kinetic energy at the entrance of the CV. [W/kg/s]
+        """
+        return self._ekin_e
+
+
+    # Mass flow and area:
+    @property
+    def mflow_e(self):
+        """
+        Mass flow at the entrance of the CV. [kg/s]
+        """
+        return self._mflow_e
+
+
+    @property
+    def A_e(self):
+        """
+        Mass flow at the entrance of the CV. [kg/s]
+        """
+        return self._A_e
+
+
+    # **********************
+    # *** Exit of the CV *** 
+    # **********************
+    # Static thermodynamic properties:
+    @property
+    def p_s(self):
+        """
+        Static pressure at the entrance of the CV. [Pa]
+        """
+        return self._p_s
+
+
+    @property
+    def T_s(self):
+        """
+        Static temperature at the entrance of the CV. [K]
+        """
+        return self._T_s
+
+
+    @property
+    def rho_s(self):
+        """
+        Static density at the entrance of the CV. [mg/m**3]
+        """
+        return self._rho_s
+
+
+    @property
+    def h_s(self):
+        """
+        Specific static enthalpy at the entrance of the CV. [W/kg/s]
+        """
+        return self._h_s
+
+
+    # Stagnation thermodynamic properties
+    @property
+    def p_st(self):
+        """
+        Stagnation pressure at the entrance of the CV. [Pa]
+        """
+        return self._p_st
+
+
+    @property
+    def T_st(self):
+        """
+        Stagnation temperature at the entrance of the CV. [K]
+        """
+        return self._T_st
+
+
+    @property
+    def rho_st(self):
+        """
+        Stagnation density at the entrance of the CV. [kg/m**3]
+        """
+        return self._rho_st
+
+
+    @property
+    def h_st(self):
+        """
+        Specific stagnation enthalpy at the entrance of the CV. [W/kg/s]
+        """
+        return self._h_st
+
+
+    # Velocity and kinetic energy:
+    @property
+    def vel_s(self):
+        """
+        Flow velocity at the entrance of the CV. [m/s]
+        """
+        return self._vel_s
+
+
+    @property
+    def mach_s(self):
+        """
+        Mach number at the entrance of the CV. [dimensionless]
+        """
+        return self._mach_s
+
+
+    @property
+    def ekin_s(self):
+        """
+        Specific kinetic energy at the entrance of the CV. [W/kg/s]
+        """
+        return self._ekin_s
+    
+    
+    # Mass flow and area:
+    @property
+    def mflow_s(self):
+        """
+        Mass flow at the entrance of the CV. [kg/s]
+        """
+        return self._mflow_s
+
+
+    @property
+    def A_s(self):
+        """
+        Area at the entrance of the CV. [m**2]
+        """
+        return self._A_s
+
+
+    # ***************************
+    # *** Transfer variables: *** 
+    # ***************************
+    @property
+    def w_se(self):
+        """
+        Speficic mechanical power along the CV. [W/mg/s]
+        """
+        return self._w_se
+
+
+    @property
+    def q_se(self):
+        """
+        Speficic heating power along the CV. [W/mg/s]
+        """
+        return self._q_se
+
+
+    @property
+    def delta_massflow(self):
+        """
+        Mass flow variation along the CV. [W/mg/s]
+        """
+        return self._delta_massflow
+
+
+    @property
+    def adiab_efficiency(self):
+        """
+        Adiabatic efficiency along the CV. [dimensionless]
+        """
+        return self._adiab_efficiency
+
+
+    ## Initializes the nozzle with the exit properties of a CV:
+    def initialize_from_cv(self):
+        """
+        Initialize nozzle from the exit properties of last CV.
+        """
+
+        return
+
