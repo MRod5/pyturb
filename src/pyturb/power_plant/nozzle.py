Astar and T,p at the throat are only calculated if the flow is supersonic, otherwise it does not make sense. Bug detected in Me calculation with sympy (equation is wrong) and now sympy is struggling to solve solvetes

src/pyturb/power_plant/nozzle.py
@@ -437,11 +437,13 @@ class Nozzle(ControlVolume):
         else:
             # If the area is provided, the mach number is calculated
             gamma_to = self.fluid.gamma(self.T_et)
+
+            # XXX: Cambiar este solveset por la solucion iterada
             var_aux = self.mflow_e/self.A_e*np.sqrt(self.fluid.Rg/gamma_to) * np.sqrt(self.T_et)/self.p_et
-            var_aux = var_aux ** (-2*(gamma_to-1)/(gamma_to+1))
+            expon = -(gamma_to+1)/(gamma_to-1)/2
 
             Me = symbols('Me')
-            ec1 = Eq(var_aux, (Me+(1+(gamma_to-1)/2*Me**2) ) )
+            ec1 = Eq(var_aux, Me*(1+(gamma_to-1)/2*Me**2)**expon)
 
             M_e = solveset(ec1, Me, domain=S.Reals)
             M_e_ = list(M_e)
@@ -504,8 +506,14 @@ class Nozzle(ControlVolume):
 
             if self.T_s <= 2/(gamma_to + 1)*self.T_st:
                 self._exit_regime = 'supersonic'
+                self._T_s_star = self.isent_flow.stat_temp_from_mach(1, self.T_st)
+                self._p_s_star = self.isent_flow.stat_pressure_from_mach(1, self.p_st, self.T_st)
+                self._A_star = self.A_s*self.mach_s*((gamma_to+1)/2/(1+(gamma_to-1)/2*self.mach_s**2))**((gamma_to+1)/2/(gamma_to-1))
             else:
                 self._exit_regime = 'subsonic'
+                self._T_s_star = np.nan
+                self._p_s_star = np.nan
+                self._A_star = np.nan
 
             self._vel_s = self.isent_flow.vel_from_stag_temp(self.T_st, self.T_s)
             self._mach_s = self.isent_flow.mach_number(self.vel_s, self.T_s)
@@ -514,10 +522,6 @@ class Nozzle(ControlVolume):
 
             self._rho_s = self.p_s / self.fluid.Rg / self.T_s
             self._A_s = self.mflow_s / self.rho_s / self.vel_s
-
-            self._T_s_star = self.isent_flow.stat_temp_from_mach(1, self.T_st)
-            self._p_s_star = self.isent_flow.stat_pressure_from_mach(1, self.p_st, self.T_st)
-            self._A_star = self.A_s*self.mach_s*((gamma_to+1)/2/(1+(gamma_to-1)/2*self.mach_s**2))**((gamma_to+1)/2/(gamma_to-1))
 
             self._rho_st = self.isent_flow.stag_density_from_mach(self.mach_s, self.rho_s, self.T_s)
             self._ekin_s = 0.5 * self.vel_s**2
@@ -534,17 +538,22 @@ class Nozzle(ControlVolume):
 
             if self.T_s <= 2/(gamma_to + 1)*self.T_st:
                 self._exit_regime = 'supersonic'
+                self._T_s_star = self.isent_flow.stat_temp_from_mach(1, self.T_st)
+                self._p_s_star = self.isent_flow.stat_pressure_from_mach(1, self.p_st, self.T_st)
+                self._A_star = self.A_s*self.mach_s*((gamma_to+1)/2/(1+(gamma_to-1)/2*self.mach_s**2))**((gamma_to+1)/2/(gamma_to-1))
+
             else:
                 self._exit_regime = 'subsonic'
+                self._T_s_star = np.nan
+                self._p_s_star = np.nan
+                self._A_star = np.nan
 
             self._vel_s = self.isent_flow.vel_from_stag_temp(self.T_st, self.T_s)
             self._mach_s = self.isent_flow.mach_number(self.vel_s, self.T_s)
             self._p_st = self.isent_flow.stag_pressure_from_mach(self.mach_s, self.p_s, self.T_s)
             self._rho_s = self.p_s / self.fluid.Rg / self.T_s
 
-            self._T_s_star = self.isent_flow.stat_temp_from_mach(1, self.T_st)
-            self._p_s_star = self.isent_flow.stat_pressure_from_mach(1, self.p_st, self.T_st)
-            self._A_star = self.A_s*self.mach_s*((gamma_to+1)/2/(1+(gamma_to-1)/2*self.mach_s**2))**((gamma_to+1)/2/(gamma_to-1))
+
 
             self._rho_st = self.isent_flow.stag_density_from_mach(self.mach_s, self.rho_s, self.T_s)
             self._ekin_s = 0.5 * self.vel_s**2
@@ -554,8 +563,31 @@ class Nozzle(ControlVolume):
             adiab_eff = (self.T_s/self.T_et-1)/((self.p_s/self.p_et)**((gamma_to-1)/gamma_to)-1)
             self._adiab_efficiency = adiab_eff
             if not 0<=adiab_eff<=1:
-                warnings.warn('Unfeasible nozzle adiabatic efficiency (ad_eff={0}) for adapted nozzle (ps={1}) and fixed area (As={2})'.format(self.adiab_efficiency, self.p_s, self.T_s), UserWarning)
+                warnings.warn('Unfeasible nozzle adiabatic efficiency (ad_eff={0}) for adapted nozzle (ps={1}) and fixed area (As={2})'.format(self.adiab_efficiency, self.p_s, self.A_s), UserWarning)
             
+
+        if self.exit_regime=='supersonic':
+            expon = -(gamma_to+1)/(2*(gamma_to-1))
+            rel_area = self.A_star/self.A_s
+            # XXX: Obtener mach con relacion de areas
+
+            M_s = symbols('M_s')
+            ec1 = Eq(rel_area, M_s * ((2+(gamma_to-1)*M_s**2)/(gamma_to+1))**expon)
+
+            M_s_ = solveset(ec1, M_s, domain=S.Reals)
+            print(M_s_)
+            M_s_ = list(M_s_)
+            M_s_.sort(reverse=True)
+            print(M_s_)
+            mach_e_value = M_s_[0] if M_s_[0]>0 else None
+
+            if mach_e_value is None:
+                raise ValueError("Mach number at entrance of the nozzle is not possitive: {0}".format(mach_e_value))
+            else:
+                self._mach_e = float(mach_e_value)
+
+            # XXX: Presion de bloqueo y presion de tobera adaptada bloqueada
+
         return
     
 
@@ -677,20 +709,20 @@ class Nozzle(ControlVolume):
         """
 
         # Nozzle type
-        if nozzle_type.lower()=='condi' or nozzle_type.lower()=='laval'
-            nozzle_type_ = 'con-di'
+        if nozzle_type.lower()=='condi' or nozzle_type.lower()=='laval':
+            nozzle_type = 'con-di'
         elif nozzle_type.lower()=='con':
             nozzle_type = 'convergent'
         elif not nozzle_type.lower() in ['con-di', 'convergent']:
             warnings.warn('Unknown nozzle type: {}. Nozzle will be set to con-di (default)'.format(nozzle_type))
         
 
-        # Store static pressure
-        self._p_s = ps
         gamma_to = self.fluid.gamma(self.T_et)
 
         if As is None:
             # Calculate discharge area assuming known adiabatic efficiency and static pressure
+            # Store static pressure and efficiency:
+            self._p_s = ps
             self._adiab_efficiency = adiab_efficiency
 
             Ts_Tet =(1 + self.adiab_efficiency*((self.p_s/self.p_et)**((gamma_to-1)/gamma_to)- 1))
@@ -718,6 +750,7 @@ class Nozzle(ControlVolume):
             self._h_s = self.h_st - self.ekin_s
 
         else:
+            # XXX OJO CAMBIAR, AQUI SE CONOCE EL AREA Y LA EFICIENCIA PORQUE CONSTRUYES LA TOBERA, DESPEJAS PS
             # As is provided, check the adiabatic efficiency
             self._A_s = As
 
@@ -751,7 +784,7 @@ class Nozzle(ControlVolume):
                 warnings.warn('Unfeasible nozzle adiabatic efficiency (ad_eff={0}) for adapted nozzle (ps={1}) and fixed area (As={2})'.format(self.adiab_efficiency, self.p_s, self.T_s), UserWarning)
             
 
-        if self.exit_regime=='supersonic' and nozzle_type='convergent'
+        if self.exit_regime=='supersonic' and nozzle_type=='convergent':
             # If the nozzle is convergent and supersonic, maximum mach number is 1
             # From the section where the nozzle is choked to the exit, the nozzle acts as a diffuser
             vel_star = self.isent_flow.vel_from_mach(1, self.T_s_star)