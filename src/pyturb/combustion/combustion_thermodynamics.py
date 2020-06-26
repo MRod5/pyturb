a0d062133a91a6a1f1ed0d7666e0d53d2b8ba995
MRodriguez <marcosrodriguezjimenez@gmail.com>
2020-06-26 21:03:19 +0200

    stoichiometry depending on the oxidizer and the fuel selected

--- src/pyturb/combustion/combustion_thermodynamics.py
+++ src/pyturb/combustion/combustion_thermodynamics.py
@@ -32,9 +32,69 @@ class ThermoCombustion(object):
         self.fuel = fuel
         self.oxidizer = oxidizer
 
+
+        reactants_status = self._classify_reactants()
+
+        if not reactants_status:
+
+            raise ValueError("Unknown fuel and oxidizer")
+
+
         return
 
 
+    def _classify_reactants(self):
+        """
+        """
 
-        
+        if not self.oxidizer.gas_species in self.oxidizer_list:
+            warnings.warn("Requested oxidizer ({0}) not available. Available oxidizers: {1}".format(self.oxidizer.species, self.oxidizer_list))
+            return False
+    
+        if not self.fuel.gas_species in self.fuel_list:
+            warnings.warn("Requested fuel ({0}) not available. Available fuels: {1}".format(self.fuel.species, self.fuel_list))
+            return False
+
+        return True
+
+    def stoichiometry(self):
+        """
+        """
+
+        alpha = 0
+        beta = 0
+        gamma = 0
+        self.stoichiometric_reaction = ''
+        reactants = ''
+        product = ''
+        print(self.fuel.thermo_prop.chemical_formula)
+        for element in self.fuel.thermo_prop.chemical_formula:
+            if element is "C":
+                alpha = self.fuel.thermo_prop.chemical_formula[element]
+                reactants += "C{0:1.0f}".format(alpha) if not alpha==0 else self.stoichiometric_reaction
+            
+            elif element is "H":
+                beta = self.fuel.thermo_prop.chemical_formula[element]
+                reactants += "H{0:1.0f}".format(beta) if not beta==0 else self.stoichiometric_reaction
+            
+            elif element is "O":
+                gamma = self.fuel.thermo_prop.chemical_formula[element]
+                reactants += "O{0:1.0f}".format(gamma) if not gamma==0 else self.stoichiometric_reaction
+
+        oxidizer_fuel_ratio = alpha + beta/4 - gamma/2
+
+        if self.oxidizer.gas_species is "Air":
+            delta = 1
+
+            reactants += " + {0:1.3f}(O2 + 79/21 N2)".format(oxidizer_fuel_ratio)
+
+        else:
+            delta = 0
+            
+            reactants += " + {0:1.3f} O2".format(oxidizer_fuel_ratio)
+
+        self.alpha = alpha
+        self.beta = beta
+        self.gamma = gamma
+        self.delta = delta
 
