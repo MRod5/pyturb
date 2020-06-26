a6a79bf923ba499a5cd01f7d5bd9ff90823617d7
MRodriguez <marcosrodriguezjimenez@gmail.com>
2020-06-26 21:02:20 +0200

    Init with fuel and oxidizer Gases (semiperfect or perfect)

--- src/pyturb/combustion/combustion_thermodynamics.py
+++ src/pyturb/combustion/combustion_thermodynamics.py
@@ -1,2 +1,40 @@
 """
 """
+from pyturb.gas_models.perfect_ideal_gas import PerfectIdealGas
+from pyturb.gas_models.semiperfect_ideal_gas import SemiperfectIdealGas
+import warnings
+
+oxidizers = ['Air', 'O2', 'O3', 'O2(L)', 'O3(L)']
+fuels = ['hydrocarbon', 'C8H18,isooctane', '']
+
+class ThermoCombustion(object):
+    """
+    """
+
+    def __init__(self, fuel, oxidizer):
+        """
+        """
+
+        if not(isinstance(fuel, PerfectIdealGas) or isinstance(fuel, SemiperfectIdealGas) or isinstance(fuel, IdealLiquid)):
+            # Check the flow is a Perfect or a Semiperfect gas fom pyturb
+            raise TypeError("Object must be PerfectIdealGas, SemiperfectIdealGas or PerfectLiquid. Instead received {}".format(fluid))
+
+
+        if not(isinstance(fuel, PerfectIdealGas) or isinstance(fuel, SemiperfectIdealGas) or isinstance(fuel, IdealLiquid)):
+            # Check the flow is a Perfect or a Semiperfect gas from pyturb
+            raise TypeError("Object must be PerfectIdealGas, SemiperfectIdealGas or PerfectLiquid. Instead received {}".format(fluid))
+
+        
+        self.oxidizer_list = oxidizers
+        self.fuel_list = fuels
+
+
+        self.fuel = fuel
+        self.oxidizer = oxidizer
+
+        return
+
+
+
+        
+
