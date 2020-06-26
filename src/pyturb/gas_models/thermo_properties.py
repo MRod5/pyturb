bca8d9ad83e090550eef155f9ffafe7e55d110bf
MRodriguez <marcosrodriguezjimenez@gmail.com>
2020-06-26 21:01:13 +0200

    Added chemical formula from Nasa Glenn coefficients file

--- src/pyturb/gas_models/thermo_properties.py
+++ src/pyturb/gas_models/thermo_properties.py
@@ -26,11 +26,13 @@ class ThermoProperties():
         
     def __str__(self):
         printable_thermoprop = "Species: {0}\tMg={1} g/mol\tdeltaHf_ref={2} J/mol\tdeltaHf_0K={3} J/mol\n".format(self.species, self.Mg, self.deltaHf_ref, self.deltaHf_0K)
+        printable_thermoprop += "-->Chemical formula: {}\n".format(self.chemical_formula)
         for ii in range(self.temp_intervals):
             printable_thermoprop += "-->Tinterval: [{0}:{1}] K\n".format(self.temp_range[ii,0], self.temp_range[ii,1])
             printable_thermoprop += "   Coefficients:  order -2  |  order -1  |  order 0  |  order 1  |  order 2  |  order 3  |  order 4\n".format()
-            printable_thermoprop += "                 {0:10.3e}   {1:10.3e}  {2:10.3e}  {3:10.3e}  {4:10.3e}  {5:10.3e}  {6:10.3e}".format(*self.coefficients[ii])
-            printable_thermoprop += "\n\n"
+            printable_thermoprop += "                 {0:10.3e}   {1:10.3e}  {2:10.3e}  {3:10.3e}  {4:10.3e}  {5:10.3e}  {6:10.3e}\n".format(*self.coefficients[ii])
+            printable_thermoprop += "\n"
+        
             
             
         return printable_thermoprop
@@ -45,6 +47,7 @@ class ThermoProperties():
             self.temp_intervals = None
             self.coefficients = None
             self.deltaHf_0K = None
+            self.chemical_formula = {}
             
 
             while keep_searching:
@@ -63,6 +66,15 @@ class ThermoProperties():
                     self.temp_intervals = int(data[0:2])
                     self.Mg = float(data[52:65])
                     self.deltaHf_ref = float(data[65:80])
+                    formula = data[10:50]
+                    
+                    for i_element in range(5):
+                        element = formula[8*(i_element):8*(i_element+1)]
+                        atom = element[0:2]
+                        if not atom=='  ':
+                            n_atoms = float(element[2:])
+                            atom = atom.strip()
+                            self.chemical_formula[atom] = n_atoms
 
                     self.temp_range = np.zeros([self.temp_intervals, 2])
                     self.coefficients = np.zeros([self.temp_intervals, 7])
@@ -74,9 +86,7 @@ class ThermoProperties():
                         coeffs_line_2 = nasa_file.readline()
 
                         self.temp_range[ii][:]=[1,2]
-
                         self.temp_range[ii,:] = [float(temprange_data[0:11]), float(temprange_data[11:22])]
-
                         self.coefficients[ii,:] = [float(coeffs_line_1[0:16].replace('D','E')), float(coeffs_line_1[16:32].replace('D','E')),
                                         float(coeffs_line_1[32:48].replace('D','E')), float(coeffs_line_1[48:64].replace('D','E')),
                                         float(coeffs_line_1[64:80].replace('D','E')), float(coeffs_line_2[0:16].replace('D','E')),
