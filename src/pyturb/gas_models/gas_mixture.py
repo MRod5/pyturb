e429491c021962e754d710173d91b829b972231c
MRodriguez <marcosrodriguezjimenez@gmail.com>
2020-07-03 19:25:52 +0200

    Gas mixture inherits from Gas class. add_gas will expand a dictionary with the moles, species and thermodynamic properties of the pure gases of the mixture

--- src/pyturb/gas_models/gas_mixture.py
+++ src/pyturb/gas_models/gas_mixture.py
@@ -1,8 +1,10 @@
 """
 """
+import pyturb.utils.constants as cts
 from pyturb.gas_models.gas_mixture import Gas
 from pyturb.gas_models.perfect_ideal_gas import PerfectIdealGas
 from pyturb.gas_models.semiperfect_ideal_gas import SemiperfectIdealGas
+import numpy as np
 import warnings
 
 
@@ -22,12 +24,18 @@ class GasMixture(object):
         else:
             self.gas_model = None
             raise ValueError("gas_model may be 'perfect' or 'semi-perfect', instead received {}".format(gas_model))
-    
+        
+
+        self.pure_substance = {}
 
         # Mixture initialization:
         if not mixture is None:
             if not type(mixture) is dict:
                 warnings.warn("mixture must be a dictionary with keys:=species and value:=moles. Instead received {}".format(mixture))
+            else:
+                #TODO: call add_gas for each pure substance in the mixture
+                print("")
+
 
         return
 
@@ -35,4 +43,35 @@ class GasMixture(object):
     def add_gas(self, species, moles):
         """
         """
+
+        pure_substance = self.gas_model(species)
+
+        self.pure_substance[pure_substance.gas_species] = {'subtance_props': pure_substance,'moles': moles}
+
+        self._get_mixture_properties()
+
         return
+
+
+    def _get_mixture_properties(self):
+        """
+        """
+        self._Nmix = np.sum(self.pure_subtance.value())
+        return
+
+    
+    @property
+    def Ru(self):
+        """
+        Get the Ideal Gas Law constant Ru [J/mol/K]
+        """
+        
+        Ru = cts.Ru
+        return Ru
+
+    
+    @property
+    def Nmix(self):
+        """
+        """
+        return self._Nmix
